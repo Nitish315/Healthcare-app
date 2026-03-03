ebce2799f14988c818b649c34ca9b43f3f39ff59
Nitish315 <149555181+Nitish315@users.noreply.github.com>
2026-03-03 10:18:29 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Facility from "../components/Static/Facility/Facility";
 import ShortIntro from "../components/Static/ShortIntro/ShortIntro";
 import WhyChoose from "../components/Static/WhyChoose/WhyChoose";
 import ContactMessage from "../components/Static/ContactMessage/ContactMessage";
+import PatientReviews from "../components/Static/PatientReviews/PatientReviews";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Facility/>
       <ShortIntro/>
       <WhyChoose/>
+      <PatientReviews/>
       <ContactMessage/>
     </>
   );
